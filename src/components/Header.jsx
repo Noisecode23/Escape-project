bd7fc834128c445c06c8cd06bfde59854bf843fb
Ankit <gamerstark210@gmail.com>
2024-05-24 22:39:41 +0530

    Update header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
       <div className='nav-items'>
         <a href="#"><span className='w-5 h-5 block rounded-full bg-zinc-100'></span></a>
       </div>
-      <div className='logo font-["Molitor_Display"] font-semibold uppercase text-[2vw]'><h1>Escape</h1></div>
+      <div className='logo fixed font-["Molitor_Display"] ml-10 font-semibold uppercase text-[2vw]'><h1>Escape</h1></div>
     </div>
     </>
   )
